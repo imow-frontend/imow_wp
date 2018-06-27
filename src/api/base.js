f01a505f396921b06df3f827318b762c9628145d
wspandihai <2711002357@qq.com>
2018-06-27 21:42:08 +0800

    fix(index-lit):fix wpy repeat bug,nothing can help.the orign bug will fix at 2.0

--- src/api/base.js
+++ src/api/base.js
@@ -1,8 +1,8 @@
 import http from '../utils/Http'
 
 export default class base {
-  // static baseUrl = 'http://mock.eolinker.com/Weyn51E7208ea8ef613195751c684d14ff3c275a43d97a8?uri=';   // wepy.$instance.globalData.baseUrl;
-  static baseUrl = 'http://localhost:9191';
+  static baseUrl = 'http://mock.eolinker.com/Weyn51E7208ea8ef613195751c684d14ff3c275a43d97a8?uri=';   // wepy.$instance.globalData.baseUrl;
+  // static baseUrl = 'http://localhost:9191';
   static get = http.get.bind(http);
   static put = http.put.bind(http);
   static post = http.post.bind(http);
