d1d81cc05ade1025b466ee87bcda3b1835c74f32
wspandihai <2711002357@qq.com>
2018-07-11 15:20:00 +0800

    chore(base):change base api url

--- src/api/base.js
+++ src/api/base.js
@@ -2,7 +2,7 @@ import http from '../utils/Http'
 
 export default class base {
   // static baseUrl = 'http://mock.eolinker.com/Weyn51E7208ea8ef613195751c684d14ff3c275a43d97a8?uri=';   // wepy.$instance.globalData.baseUrl;
-  static baseUrl = 'http://192.168.1.36:9191';
+  static baseUrl = 'http://192.168.1.183:9191';
   static get = http.get.bind(http);
   static put = http.put.bind(http);
   static post = http.post.bind(http);
